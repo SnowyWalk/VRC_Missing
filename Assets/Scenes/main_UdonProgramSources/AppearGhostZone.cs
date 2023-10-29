bbc668804ba9a1d82ffc015fea6bc848294aaea3
SnowyWalk <chunsa1166jp@gmail.com>
2023-10-29 23:19:00 +0900

    Import TMPro for Better Text UI. Make example button and zone.

---
+++ Assets/Scenes/main_UdonProgramSources/AppearGhostZone.cs
@@ -0,0 +1,32 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class AppearGhostZone : UdonSharpBehaviour
+{
+    public GameObject ghost;
+
+
+
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        base.OnPlayerTriggerEnter(player);
+
+        ghost.SetActive(true);
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        base.OnPlayerTriggerExit(player);
+
+        ghost.SetActive(false);
+    }
+
+    public override void OnPlayerTriggerStay(VRCPlayerApi player)
+    {
+        base.OnPlayerTriggerStay(player);
+    }
+}
---
+++ Assets/Scenes/main_UdonProgramSources/ExampleButton.cs
@@ -0,0 +1,48 @@
+﻿
+using TMPro;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ExampleButton : UdonSharpBehaviour
+{
+    public GameObject anotherObj;
+
+
+    public TextMeshProUGUI subtitle;
+
+
+    float timer = 0.0f;
+
+
+    public override void Interact()
+    {
+        subtitle.text = "What is going on?";
+        subtitle.gameObject.SetActive(true);
+    }
+
+
+
+    private void Update()
+    {
+        // play in 60FPS -> Time.deltaTime will be 0.016 sec
+
+        if (subtitle.gameObject.activeSelf == false) // if inactivated
+        {
+            timer = 0.0f; // reset
+        }
+        else 
+        {
+            timer += Time.deltaTime; // accumulate
+
+            if (timer > 3.0f) // over 3 second
+            {
+                // deactivate subtitle
+                subtitle.gameObject.SetActive (false);
+            }
+        }
+        
+    }
+
+}
